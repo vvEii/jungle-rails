005f9724d633884b0b8dfeb713cac987a1c88d50
vvEii <coopershang125@gmail.com>
2020-11-17 18:29:50 -0800

    Implement user-auth functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-
+    session[:user_id] = nil
+    redirect_to '/login'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,9 +23,9 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-    
-  get '/signup', to: 'user#new'
-  get '/users', to: 'users#create'
+
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
